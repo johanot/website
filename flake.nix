1b1f9e02ea9420a82b0f5a81745a9dd69596f8a6
Johan Thomsen <write@ownrisk.dk>
2023-08-08 23:23:29 +0200

    add code

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "website";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+  };
+
+  outputs = { self, nixpkgs, ... }@attrs:
+  let
+    pname = "website";
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+      ];
+    };
+    lib = nixpkgs.lib;
+  in {
+    inherit nixpkgs;
+
+    devShell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        mkdocs
+        python3Packages.mkdocs-material
+      ];
+    };
+  };
+}
